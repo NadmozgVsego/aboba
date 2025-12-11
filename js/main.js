28a2fc129178c14c6c71c84540b859161b3a6c51
NadmozgVsego <60742911+NadmozgVsego@users.noreply.github.com>
2025-12-12 02:57:26 +0300

    lesson 3

---
+++ js/main.js
@@ -0,0 +1,39 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  slidesPerView: 6,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  spaceBetween: 10,
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+/* старый скрипт */
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+
+//   // If we need pagination
+//   // pagination: {
+//   //   el: '.swiper-pagination',
+//   // },
+
+//   // Navigation arrows
+//   // navigation: {
+//   //   nextEl: '.swiper-button-next',
+//   //   prevEl: '.swiper-button-prev',
+//   // },
+
+//   // And if we need scrollbar
+//   //   scrollbar: {
+//   //     el: '.swiper-scrollbar',
+//   //   },
+// });
